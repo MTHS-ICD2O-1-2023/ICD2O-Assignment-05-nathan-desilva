4b1151b475be366319dc82606d5307f08c16f04d
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-05-26 19:43:11 +0000

    added favi images html css and js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Nathan De Silva All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Nathan De Silva
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  // Input
+  let numberRepeat = parseInt(document.getElementById("number-repeat").value)
+
+  // Process
+  let numerator = 4
+  let denominator = 1
+  let counter = 0
+  let result = 4
+
+  while (counter < numberRepeat - 1) {
+    denominator += 2
+    numerator = numerator * -1  // Alternate the sign of numerator
+    result = result + (numerator / denominator)
+    counter += 1
+  }
+
+  // Output
+  document.getElementById("answer").innerHTML = "Your result is ≈ " + result.toFixed(4)
+}
